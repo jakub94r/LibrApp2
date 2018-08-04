4e754a9f87cbc03aa11a87bd8a59747db28009df
Kuba <Kuba@DESKTOP-5CGD7OF>
2018-08-04 18:21:43 +0200

    Add project files.

---
+++ LibrApp2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LibrApp2.Startup))]
+namespace LibrApp2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
